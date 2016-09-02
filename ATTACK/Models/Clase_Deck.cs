cf6448922590259cff0463e64b9ce9bcba4d50b6
ImmortalGG <ronix05@hotmail.com>
2016-09-02 15:39:03 -0600

    Ingresadas las funciones relacionadas al DECK

--- ATTACK/Models/Clase_Deck.cs
+++ ATTACK/Models/Clase_Deck.cs
@@ -18,7 +18,85 @@ namespace ATTACK.Models
         public Clase_Carta _ID_Carta { get { return ID_Carta; } set { ID_Carta = value; } }
         public int _Cantidad_Cartas { get { return Cantidad_Cartas; } set { Cantidad_Cartas = value; } }
 
+        public static int Insertar_Usuario(String _Nombre_Usuario, String _Nombre_Deck, int _ID_Carta, int _Cantidad_Cartas) //inserta un deck
+        {
+            int respuesta = 0;
 
+            Conexion conx_detalles = new Conexion();
+            conx_detalles.parametro();
+            conx_detalles.inicializa();
+            string CONSULTA;
+            System.Data.OleDb.OleDbDataReader CONTENEDOR;
 
+            CONSULTA = "EXEC INGRESAR_DECK ?,?,?,?";
+            conx_detalles.annadir_consulta(CONSULTA);
+            conx_detalles.annadir_parametro(_Nombre_Usuario, 2);
+            conx_detalles.annadir_parametro(_Nombre_Deck, 2);
+            conx_detalles.annadir_parametro(_ID_Carta, 1);
+            conx_detalles.annadir_parametro(_Cantidad_Cartas, 1);
+            CONTENEDOR = conx_detalles.busca();
+            while (CONTENEDOR.Read())
+            {
+                respuesta = Convert.ToInt32(CONTENEDOR[0].ToString());
+            }
+            conx_detalles.conexion.Close();
+            conx_detalles.conexion.Dispose();
+            CONTENEDOR.Close();
+            return respuesta;
+        }
+
+        public static int Modificar_Usuario(String _Nombre_Usuario, String _Nombre_Deck, int _ID_Carta, int _Cantidad_Cartas) //modificar un deck
+        {
+            int respuesta = 0;
+
+            Conexion conx_detalles = new Conexion();
+            conx_detalles.parametro();
+            conx_detalles.inicializa();
+            string CONSULTA;
+            System.Data.OleDb.OleDbDataReader CONTENEDOR;
+
+            CONSULTA = "EXEC MODIFICAR_DECK ?,?,?,?";
+            conx_detalles.annadir_consulta(CONSULTA);
+            conx_detalles.annadir_parametro(_Nombre_Usuario, 2);
+            conx_detalles.annadir_parametro(_Nombre_Deck, 2);
+            conx_detalles.annadir_parametro(_ID_Carta, 1);
+            conx_detalles.annadir_parametro(_Cantidad_Cartas, 1);
+            CONTENEDOR = conx_detalles.busca();
+            while (CONTENEDOR.Read())
+            {
+                respuesta = Convert.ToInt32(CONTENEDOR[0].ToString());
+            }
+            conx_detalles.conexion.Close();
+            conx_detalles.conexion.Dispose();
+            CONTENEDOR.Close();
+            return respuesta;
+        }
+
+        public static int Eliminar_Usuario(String _Nombre_Usuario, String _Nombre_Deck, int _ID_Carta, int _Cantidad_Cartas) //eliminar un deck
+        {
+            int respuesta = 0;
+
+            Conexion conx_detalles = new Conexion();
+            conx_detalles.parametro();
+            conx_detalles.inicializa();
+            string CONSULTA;
+            System.Data.OleDb.OleDbDataReader CONTENEDOR;
+
+            CONSULTA = "EXEC ELIMINAR_DECK ?,?,?,?";
+            conx_detalles.annadir_consulta(CONSULTA);
+            conx_detalles.annadir_parametro(_Nombre_Usuario, 2);
+            conx_detalles.annadir_parametro(_Nombre_Deck, 2);
+            conx_detalles.annadir_parametro(_ID_Carta, 1);
+            conx_detalles.annadir_parametro(_Cantidad_Cartas, 1);
+            CONTENEDOR = conx_detalles.busca();
+            while (CONTENEDOR.Read())
+            {
+                respuesta = Convert.ToInt32(CONTENEDOR[0].ToString());
+            }
+            conx_detalles.conexion.Close();
+            conx_detalles.conexion.Dispose();
+            CONTENEDOR.Close();
+            return respuesta;
+        }
     }
 }
