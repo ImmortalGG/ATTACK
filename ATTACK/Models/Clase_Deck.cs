ce2dc0334784d21b75566600d863a8b6a9ff8f89
ImmortalGG <ronix05@hotmail.com>
2016-09-02 12:37:41 -0600

    Agregada la clase DECK

---
+++ ATTACK/Models/Clase_Deck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATTACK.Models
+{
+    public class Clase_Deck
+    {
+        private string Nombre_Usuario;
+        private string Nombre_Deck;
+        private int ID_Carta;
+        private int Cantidad_Cartas;
+
+        public Clase_Deck() { }
+        public string _Nombre_Usuario { get { return Nombre_Usuario; } set { Nombre_Usuario = value; } }
+        public string _Nombre_Deck { get { return Nombre_Deck; } set { Nombre_Deck = value; } }
+        public int _ID_Carta { get { return ID_Carta; } set { ID_Carta = value; } }
+        public int _Cantidad_Cartas { get { return Cantidad_Cartas; } set { Cantidad_Cartas = value; } }
+
+
+    }
+}
