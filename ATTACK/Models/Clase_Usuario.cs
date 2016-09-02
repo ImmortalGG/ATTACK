691eb3bc26339c756d27584273746cfa77472953
ImmortalGG <ronix05@hotmail.com>
2016-09-02 12:49:20 -0600

    Agregada la clase USUARIO

---
+++ ATTACK/Models/Clase_Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATTACK.Models
+{
+    public class Clase_Usuario
+    {
+        private string Nombre;
+        private string Correo;
+        private string Contra;
+        private int Tipo;
+
+        public Clase_Usuario() { }
+
+        public string _Nombre { get { return Nombre; } set { Nombre = value; } }
+        public string _Correo { get { return Correo; } set { Correo = value; } }
+        public string _Contra { get { return Contra; } set { Contra = value; } }
+        public int _Tipo { get { return Tipo; } set { Tipo = value; } }
+    }
+}
