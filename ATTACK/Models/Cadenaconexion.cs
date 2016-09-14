4e7a227a6c1c443e7f704a906050c7888f00bdb6
ImmortalGG <ronix05@hotmail.com>
2016-09-14 15:58:15 -0600

    BUGS EVERYWHERE

--- ATTACK/Models/Cadenaconexion.cs
+++ ATTACK/Models/Cadenaconexion.cs
@@ -22,7 +22,7 @@ namespace ATTACK.Models
                 //cadena = "Provider=SQLOLEDB;Data Source=173.201.35.40,1433;Initial Catalog=IBS_taquilla_web;Persist Security Info=True;User ID=IBS_user;Password=Dionisio2012;Pooling=False";
                 //cadena = "Provider=SQLOLEDB;Data Source=DESARROLLO-PC\\SQLDESARROLLO;Initial Catalog=IBS_taquilla_web;Integrated Security=True;Pooling=False";
                // cadena = "Provider=SQLOLEDB;Data Source=MILFOIL.arvixe.com;Initial Catalog=IBS_taquilla_web;Persist Security Info=True;User ID=mavar84;Password=Candelabro27;Pooling=False";
-                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=aprovecha2;Persist Security Info=True;User ID=desarrollo;Password=pass123";
+                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=ATTACK;Persist Security Info=True;User ID=desarrollo;Password=pass123";
               //  cadena = "Provider=SQLOLEDB;Data Source=sistemcr.com;Initial Catalog=Approvecha2;Persist Security Info=True;User ID=Mavar84;Password=Candelabro27;Pooling=False";
            
             }
@@ -31,7 +31,7 @@ namespace ATTACK.Models
                 //cadena = "Provider=SQLOLEDB;Data Source=SrvrEspressivo\\ICG;Initial Catalog=IBS_taquilla_web;Persist Security Info=True;User ID=IBS_user;Password=Dionisio2012";
                 //cadena = "Provider=SQLOLEDB;Data Source=DESARROLLO-PC\\SQLDESARROLLO;Initial Catalog=IBS_taquilla_web;Integrated Security=True;Pooling=False";
                 //cadena = "Provider=SQLOLEDB;Data Source=MILFOIL.arvixe.com;Initial Catalog=IBS_taquilla_web;Persist Security Info=True;User ID=mavar84;Password=Candelabro27;Pooling=False";
-                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=aprovecha2;Persist Security Info=True;User ID=desarrollo;Password=pass123";
+                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=ATTACK;Persist Security Info=True;User ID=desarrollo;Password=pass123";
                 //cadena = "Provider=SQLOLEDB;Data Source=sistemcr.com;Initial Catalog=Approvecha2;Persist Security Info=True;User ID=Mavar84;Password=Candelabro27;Pooling=False";
            
             }
@@ -48,13 +48,13 @@ namespace ATTACK.Models
             if (machineName == "Sisscr-PC")
             {
                 //cadena = "Provider=SQLOLEDB;Data Source=173.201.35.40,1433;Initial Catalog=Espressivo;Persist Security Info=True;User ID=IBS_user;Password=Dionisio2012;Pooling=False";
-                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=IBS_taquilla_web;Persist Security Info=True;User ID=desarrollo;Password=pass123;Pooling=False";
+                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=ATTACK;Persist Security Info=True;User ID=desarrollo;Password=pass123;Pooling=False";
                 //cadena = "Data Source=DESARROLLO-PC;Initial Catalog=IBS_taquilla_web;Integrated Security=True";
             }
             else
             {
                 //cadena = "Provider=SQLOLEDB;Data Source=SrvrEspressivo\\ICG;Initial Catalog=Espressivo;Persist Security Info=True;User ID=IBS_user;Password=Dionisio2012";
-                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=IBS_taquilla_web;Persist Security Info=True;User ID=desarrollo;Password=pass123;Pooling=False";
+                cadena = "Provider=SQLOLEDB;Data Source=192.168.59.150;Initial Catalog=ATTACK;Persist Security Info=True;User ID=desarrollo;Password=pass123;Pooling=False";
                // cadena = "Data Source=DESARROLLO-PC;Initial Catalog=IBS_taquilla_web;Integrated Security=True";
             }
             return cadena;
